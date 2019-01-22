c3dddfd6cccaf6094dc2dc2cb1e364a65e81b340
ShubhamSNegi <shubhamsinghnegi45@gmail.com>
2019-01-22 14:52:37 +0530

    app.blade. error checked

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Emailcompaignapp</title>
+    <title>{{config('app.name','Emailcompaignapp'}}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>

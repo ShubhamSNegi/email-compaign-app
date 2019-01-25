72c0b6d5e23498b52fa5de6f56efab461daa9979
ShubhamSNegi <shubhamsinghnegi45@gmail.com>
2019-01-26 00:00:50 +0530

    Indentation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,15 +17,19 @@
             </div>
         </div>
     </div>
- <div class="jumbotron jumbotron-fluid"style="margin-top:-5%;padding-top:5%;" class="container">
-  <div class="container">
-    <h1 class="display-4">Email Campaign App</h1>
-    <p class="lead">            Simply create, edit and use to transform your mail</p>
-    <div style="margin-top:7%;padding-bottom:5%;"><a href="/campaign" class="btn btn-outline-primary btn-lg" style="margin-right:3%;" tabindex="-1" role="button">Compaign</a>
- <a href="/subscriber" class="btn btn-outline-primary btn-lg" tabindex="-1" role="button"style="margin-right:3%;">Subscriber</a>
- <a href="/categories" class="btn btn-outline-primary btn-lg" tabindex="-1" role="button">Categories</a>
- <div>
-  </div>
-</div>
-
+    <div class="jumbotron jumbotron-fluid"style="margin-top:-5%;padding-top:5%;" class="container">
+         <div class="container">
+              <h1 class="display-4">Email Campaign App</h1>
+                  <p class="lead">Simply create, edit and use to transform your mail</p>
+               <div style="margin-top:7%;padding-bottom:5%;">
+                    <a href="/campaign" class="btn btn-outline-primary btn-lg" style="margin-right:3%;" tabindex="-1" role="button">
+                       Compaign
+                    </a>
+                    <a href="/subscriber" class="btn btn-outline-primary btn-lg" tabindex="-1" role="button"style="margin-right:3%;">
+                       Subscriber
+                    </a>
+                    <a href="/categories" class="btn btn-outline-primary btn-lg" tabindex="-1" role="button">
+                       Categories
+                    </a>
+               </div>
 @endsection
